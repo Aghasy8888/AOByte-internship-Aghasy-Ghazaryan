217a2a81d395ea06b1c8d2e1eb224d194abf426d
Aghasy8888 <aghasghazaryan8888@gmail.com>
2023-07-04 13:02:48 +0400

    some changes in ajax

--- Javascript/ajax.js
+++ Javascript/ajax.js
@@ -2,36 +2,27 @@ import CustomPromise from "./promise.js";
 
 export default function ajax(url, config) {
   return new CustomPromise(function (resolve, reject) {
-    try {
-      const xhr = new XMLHttpRequest();
-      xhr.open(config.type || "GET", url);
+    const xhr = new XMLHttpRequest();
+    xhr.open(config.type || "GET", url);
 
-      for (let header in config.headers) {
-        if (config.headers.hasOwnProperty(header)) {
-          xhr.setRequestHeader(header, config.headers[header]);
-        }
+    for (let header in config.headers) {
+      if (config.headers.hasOwnProperty(header)) {
+        xhr.setRequestHeader(header, config.headers[header]);
       }
-
-      xhr.onload = function () {
-        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject(new Error(xhr.statusText));
-        }
-      };
-
-      xhr.onerror = function () {
-        reject(new Error("Network error"));
-      };
-
-      try {
-        xhr.send(JSON.stringify(config.data));
-      } catch (error) {
-        console.log("Error occurred while sending request:", error);
-        reject(error);
-      }
-    } catch (error) {
-      console.log("hello this is my error.");
     }
+
+    xhr.onload = function () {
+      if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        reject(new Error(xhr.statusText));
+      }
+    };
+
+    xhr.onerror = function () {
+      reject(new Error("Network error"));
+    };
+
+    xhr.send(JSON.stringify(config.data));
   });
 }
